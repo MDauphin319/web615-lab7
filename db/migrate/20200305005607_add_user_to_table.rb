e947ba8aac5564b1e0bafdcce3f484c254fccd55
Matt Dauphin <matthew.dauphin01@stclairconnect.ca>
2020-03-05 14:13:47 -0500

    fix

--- db/migrate/20200305005607_add_user_to_table.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToTable < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :articles, :user, foreign_key: true
-    add_reference :comments, :user, foreign_key: true
-  end
-end
